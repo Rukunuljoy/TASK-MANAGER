8935ffdbbfc36da4db441e0816b6a02dbb2aae8a
Rukunuljoy <rukunuljoypub58@gmail.com>
2025-04-08 11:35:26 +0600

    first commit

---
+++ frontend/Task-Manager/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="w-25">
+      <h1
+        type="button"
+        className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer"
+      >
+        app
+      </h1>
+    </div>
+  );
+}
+
+export default App;
